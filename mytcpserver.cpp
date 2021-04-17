e81afaaa5eb53dbfe98d2a5c44023a82f4b2a779
Dybashka <72248943+Dybashka@users.noreply.github.com>
2021-04-17 14:23:05 +0300

    Add files via upload

---
+++ mytcpserver.cpp
@@ -0,0 +1,50 @@
+#include "mytcpserver.h"
+#include <QDebug>
+#include <QCoreApplication>
+
+MyTcpServer::~MyTcpServer()
+{
+    mTcpServer->close();
+    server_status=0;
+}
+
+MyTcpServer::MyTcpServer(QObject * parent) : QObject(parent)
+{
+    mTcpServer = new QTcpServer(this);
+    connect(mTcpServer, &QTcpServer::newConnection, this, &MyTcpServer::slotNewConnection);
+
+    if (!mTcpServer->listen(QHostAddress::Any, 12345))
+    {
+        qDebug() << "server is not started";//выводит если не подключился к серверу
+    }
+    else
+    {
+        server_status = 1;
+        qDebug() << "server is started"; //выводит если подключено
+    }
+}
+
+void MyTcpServer::slotNewConnection()
+{
+    if (server_status == 1)
+    {
+        mTcpSocket = mTcpServer->nextPendingConnection();
+        mTcpSocket->write("Hello, World!!! I am echo server!\r\n");
+        connect(mTcpSocket, &QTcpSocket::readyRead, this, &MyTcpServer::slotServerRead);
+        connect(mTcpSocket, &QTcpSocket::disconnected, this, &MyTcpServer::slotClientDisconnected);
+    }
+}
+
+void MyTcpServer::slotServerRead()
+{
+    while (mTcpSocket->bytesAvailable() > 0)
+    {
+        QByteArray array = mTcpSocket->readAll();
+        mTcpSocket->write(array);
+    }
+}
+
+void MyTcpServer::slotClientDisconnected()
+{
+    mTcpSocket->close();
+}
